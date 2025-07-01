8e2151c43cfc9772db5601d1b0b12ecbddeedbb5
Saltybukket <tobias.feichtner.chr@gmail.com>
2025-07-01 18:18:04 +0200

    added delta time and screen update

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+#constants must be in first line for pygame to access
+#imports all as is -> with "import constants" -> would need constants.<CONSTANT> to access
+from constants import *     
+import pygame
+
+
 def main():
-    print("Hello from asteroids!")
-
-
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    game_clock = pygame.time.Clock()
+    dt = 0      #delta time
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))     #Display dimensions as touple
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
+        screen.fill("black")
+        pygame.display.flip()       #need pygame.display.flip() instead of screen.flip() -> not a surface method
+        
+        dt = game_clock.tick(60) / 1000         #wait 1/60 sec  | retuns time since last call (delta_time)
+    
 if __name__ == "__main__":
     main()
