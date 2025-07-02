0f705dccc29459c6d6197caac73201cbb73e8e0e
Saltybukket <tobias.feichtner.chr@gmail.com>
2025-07-02 18:15:11 +0200

    added shooting

--- player.py
+++ player.py
@@ -1,9 +1,12 @@
 from circleshape import *
 from constants import *
+from shot import *
+
 class Player(CircleShape):
     def __init__(self, x ,y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
   
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -33,5 +36,13 @@ class Player(CircleShape):
             
         if keys[pygame.K_s]:
             self.move(-dt) 
- 
+            
+        if keys[pygame.K_SPACE]:
+            self.shoot() 
+    
+    def shoot(self):
+        new_shot = Shot(self.position.x, self.position.y)
+        new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        
+        
         
